0443ab4bdee3860f003d984ff602db5940ef454a
tiagofcc <tiagofccosta@ua.pt>
2024-12-10 23:51:11 +0000

    idk

---
+++ magui/countdown/contagem.js
@@ -0,0 +1,24 @@
+﻿$(document).ajaxComplete(function (event, xhr, options) {
+    $("#myModal").modal('hide');
+})
+
+
+
+
+
+const themeToggle = document.getElementById('themeToggle');
+const body = document.body;
+const navbar = document.querySelector('.navbar');
+
+themeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    if (body.classList.contains('dark-mode')) {
+        navbar.classList.remove('navbar-light-mode');
+        navbar.classList.add('navbar-dark-mode');
+        themeToggle.innerHTML = '<i class="fa fa-sun-o" aria-hidden="true"></i>';
+    } else {
+        navbar.classList.remove('navbar-dark-mode');
+        navbar.classList.add('navbar-light-mode');
+        themeToggle.innerHTML = '<i class="fa fa-moon-o" aria-hidden="true"></i>';
+    }
+});
