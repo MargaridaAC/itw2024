e5db33d4338537e7d113c35962b1c83636ab7de2
magui14x <cardoso.margarida@ua.pt>
2024-12-10 15:52:15 +0000

    ss

---
+++ magui/competitions.js
@@ -0,0 +1,117 @@
+﻿
+const translations = {
+    en: {
+        name: "Name",
+        sport_id: "Sport Id",
+    },
+    pt: {
+        name: "Nome",
+        sport_id: "Id do desporto",
+    }
+};
+
+// ViewModel KnockOut
+var vm = function () {
+    console.log('ViewModel initiated...');
+    var self = this;
+
+    // Observáveis
+    self.baseUri = ko.observable('http://192.168.160.58/Paris2024/api/Competitions');
+    self.displayName = 'Paris2024 Competitions List';
+    self.currentLanguage = ko.observable('en');
+    self.error = ko.observable('');
+    self.competitions = ko.observableArray([]);
+    self.currentPage = ko.observable(1);
+    self.pagesize = ko.observable(20);
+    self.totalRecords = ko.observable(50);
+    self.hasPrevious = ko.observable(false);
+    self.hasNext = ko.observable(false);
+    self.totalPages = ko.observable(0);
+
+    // Computed observables
+    self.previousPage = ko.computed(() => Math.max(1, self.currentPage() - 1));
+    self.nextPage = ko.computed(() => Math.min(self.totalPages(), self.currentPage() + 1));
+
+    self.fromRecord = ko.computed(() => (self.currentPage() - 1) * self.pagesize() + 1);
+    self.toRecord = ko.computed(() => Math.min(self.currentPage() * self.pagesize(), self.totalRecords()));
+
+    self.pageArray = function () {
+        const size = Math.min(self.totalPages(), 9);
+        const step = Math.max(0, Math.min(self.currentPage() - 5, self.totalPages() - 9));
+        return Array.from({ length: size }, (_, i) => i + 1 + step);
+    };
+
+    //obter o texto traduzido do array
+    self.getTranslation = function (key) {
+        const lang = self.currentLanguage();
+        return translations[lang][key] || key;
+    };
+
+    //trocar a linguagem fml
+    self.changeLanguage = function (lang) {
+        if (translations[lang]) {
+            self.currentLanguage(lang);
+        }
+    };
+
+    // Função para ativar a página
+    self.activate = function (id) {
+        console.log('CALL: getCompetitions...');
+        const composedUri = `${self.baseUri()}?page=${id}&pageSize=${self.pagesize()}`;
+        ajaxHelper(composedUri, 'GET').done(function (data) {
+            hideLoading();
+            self.competitions(data.Competitions);
+            self.currentPage(data.CurrentPage);
+            self.hasNext(data.HasNext);
+            self.hasPrevious(data.HasPrevious);
+            self.pagesize(data.PageSize);
+            self.totalPages(data.TotalPages);
+            self.totalRecords(data.TotalCompetitions);
+        });
+    };
+
+    // Função AJAX
+    function ajaxHelper(uri, method, data) {
+        self.error('');
+        return $.ajax({
+            type: method,
+            url: uri,
+            dataType: 'json',
+            contentType: 'application/json',
+            data: data ? JSON.stringify(data) : null,
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.error(`AJAX Call [${uri}] Fail:`, errorThrown);
+                hideLoading();
+                self.error(errorThrown);
+            }
+        });
+    }
+
+    // Funções para mostrar e esconder o loading
+    function showLoading() {
+        $("#myModal").modal('show', { backdrop: 'static', keyboard: false });
+    }
+
+    function hideLoading() {
+        $('#myModal').on('shown.bs.modal', function (e) {
+            $("#myModal").modal('hide');
+        })
+    }
+
+    // Função para obter parâmetros da URL
+    function getUrlParameter(sParam) {
+        const params = new URLSearchParams(window.location.search);
+        return params.get(sParam);
+    }
+
+    // Inicialização
+    showLoading();
+    const pg = getUrlParameter('page') || 1;
+    self.activate(pg);
+    console.log("VM initialized!");
+};
+
+$(document).ready(function () {
+    console.log("Document ready!");
+    ko.applyBindings(new vm());
+});
