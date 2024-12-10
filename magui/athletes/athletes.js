6bf4c86e000c72c137efa55a187f840e440c3c86
tiagofcc <tiagofccosta@ua.pt>
2024-12-10 23:23:05 +0000

    mais mudanças

--- magui/athletes/athletes.js
+++ magui/athletes/athletes.js
@@ -128,3 +128,36 @@ $(document).ready(function () {
 $(document).ajaxComplete(function (event, xhr, options) {
     $("#myModal").modal('hide');
 })
+
+
+//   Darkmode  // 
+
+$(document).ready(function () {
+    console.log("ready!");
+    ko.applyBindings(new vm());
+});
+
+$(document).ajaxComplete(function (event, xhr, options) {
+    $("#myModal").modal('hide');
+})
+
+
+
+
+
+const themeToggle = document.getElementById('themeToggle');
+const body = document.body;
+const navbar = document.querySelector('.navbar');
+
+themeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    if (body.classList.contains('dark-mode')) {
+        navbar.classList.remove('navbar-light-mode');
+        navbar.classList.add('navbar-dark-mode');
+        themeToggle.innerHTML = '<i class="fa fa-sun-o" aria-hidden="true"></i>';
+    } else {
+        navbar.classList.remove('navbar-dark-mode');
+        navbar.classList.add('navbar-light-mode');
+        themeToggle.innerHTML = '<i class="fa fa-moon-o" aria-hidden="true"></i>';
+    }
+});
